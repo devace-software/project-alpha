3b5b16a0f403932661b6259c91027186473add5a
Ahmad Abu Yahya <ahmadj.abuyahya@gmail.com>
2022-06-29 23:43:46 +0000

    edit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src='https://avatars.githubusercontent.com/u/45077440?s=400&u=94671ad710e5260fff775dc973c383a1b646d57b&v=4' className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You Are Awesome!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://devace.software"
           target="_blank"
           rel="noopener noreferrer"
         >
